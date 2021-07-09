c4051691a4cd47b96f9ac6051804cad03e98fb3e
ankushmahesh12 <ankushmahesh12@gmail.com>
2021-07-09 18:40:54 +0530

    installed and setup ejs

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port=8000;
 
 //use express router
 app.use('/', require('./routes'));
+app.set('view engine','ejs');
+app.set('views','./views');
 //app.use('/',require('./routes/users_route'));
 app.listen(port,function(err){
     if(err){
