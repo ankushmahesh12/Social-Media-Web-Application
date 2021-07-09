7a459f4d38761f3deebe3536a78cbe9558b006c3
ankushmahesh12 <ankushmahesh12@gmail.com>
2021-07-09 17:26:13 +0530

    home controller

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express=require('express');
 
-const router=express.Router;
+const router=express.Router();
+const home_Controller=require('../controller/home_controller');
 
-
+console.log("router");
+router.get('/', home_Controller.home);
 
 
 module.exports=router;
