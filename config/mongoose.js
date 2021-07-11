4dbc0a6c9e9b16dfbc6f3b1ce9547eafd57ecbdb
ankushmahesh12 <ankushmahesh12@gmail.com>
2021-07-11 18:01:33 +0530

    mongoose is setup and configure

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost:27017/codeial_development',{
+     useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+
+const db=mongoose.connection;
+db.on('error',console.error.bind(console, "error connecting to database"));
+db.once('open',function(){
+    console.log('Connection to database is successful');
+});
+module.exports=db;
